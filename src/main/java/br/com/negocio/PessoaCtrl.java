7b9152bf7fd8980591425ebd65d68d0ab6ec298c
mexiricas <mexiricas@hotmail.com>
2016-10-25 10:13:15 -0200

    fone funcionando

--- src/main/java/br/com/negocio/PessoaCtrl.java
+++ src/main/java/br/com/negocio/PessoaCtrl.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 import br.com.beans.Fone;
 import br.com.beans.Pessoa;
 import br.com.persistencia.PessoaDao;
 
 @ManagedBean(name = "pessoaBeans")
+@SessionScoped
 public class PessoaCtrl implements Serializable {
 
 	private static final long serialVersionUID = 7329427687235074332L;
@@ -48,9 +50,14 @@ public class PessoaCtrl implements Serializable {
 
 	public String actionInserirFone() {
 		System.out.println("passou");
-
+		Fone fone = new Fone();
 		fone.setPessoa(pessoa);
 		pessoa.getFones().add(fone);
+		return "form_pessoa";
+	}
+
+	public String actionExcluirFone(Fone f) {
+		/* os alunos ... pesquisar e codificar */
 		return "form_pessoa";
 	}
 
