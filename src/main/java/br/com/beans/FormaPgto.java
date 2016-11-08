2c5b70d58ccce8b2cb6a3852c5cf15c9e592322b
mexiricas <mexiricas@hotmail.com>
2016-11-08 20:17:58 -0200

    . classe FormPgts

---
+++ src/main/java/br/com/beans/FormaPgto.java
@@ -0,0 +1,71 @@
+package br.com.beans;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "forma_pgto")
+public class FormaPgto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "fpg_id")
+	private int id;
+	@Column(name = "fpg_descricao", length = 20, nullable = true)
+	private String descricao;
+	@Column(name = "fpg_num_max_parc", nullable = true)
+	private int numMaxParc;
+	@Column(name = "fpg_num_padrao_parc", nullable = true)
+	private int numPadraoParc;
+	@Column(name = "fpg_intervalo_dias", nullable = true)
+	private int intervaloDias;
+	@Column(name = "fpg_percentual_acres", nullable = true)
+	private float percentualAcres;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getNumMaxParc() {
+		return numMaxParc;
+	}
+
+	public void setNumMaxParc(int numMaxParc) {
+		this.numMaxParc = numMaxParc;
+	}
+
+	public int getNumPadraoParc() {
+		return numPadraoParc;
+	}
+
+	public void setNumPadraoParc(int numPadraoParc) {
+		this.numPadraoParc = numPadraoParc;
+	}
+
+	public int getIntervaloDias() {
+		return intervaloDias;
+	}
+
+	public void setIntervaloDias(int intervaloDias) {
+		this.intervaloDias = intervaloDias;
+	}
+
+	public float getPercentualAcres() {
+		return percentualAcres;
+	}
+
+	public void setPercentualAcres(float percentualAcres) {
+		this.percentualAcres = percentualAcres;
+	}
+}
---
+++ src/main/java/br/com/persistencia/FormaPgtoDAO.java
@@ -0,0 +1,64 @@
+package br.com.persistencia;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.*;
+
+import br.com.beans.FormaPgto;
+
+public class FormaPgtoDAO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public static void inserir(FormaPgto formaPgto) {
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+		Transaction t = sessao.beginTransaction();
+		sessao.save(formaPgto);
+		t.commit();
+		sessao.close();
+	}
+
+	public static void alterar(FormaPgto formaPgto) {
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+		Transaction t = sessao.beginTransaction();
+		sessao.update(formaPgto);
+		t.commit();
+		sessao.close();
+	}
+
+	public static void excluir(FormaPgto formaPgto) {
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+		Transaction t = sessao.beginTransaction();
+		sessao.delete(formaPgto);
+		t.commit();
+		sessao.close();
+	}
+
+	public static List<FormaPgto> listagem(String filtro) {
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+		Query consulta;
+		List<FormaPgto> lista = null;
+		if (filtro.trim().length() == 0) {
+			consulta = sessao
+					.createQuery("from FormaPgto order by fpg_descricao");
+		} else {
+			consulta = sessao
+					.createQuery("from FormaPgto where fpg_descricao like :parametro order by fpg_descricao");
+			consulta.setString("parametro", "%" + filtro + "%");
+		}
+		lista = consulta.list();
+		sessao.close();
+		return lista;
+	}
+
+	public static FormaPgto pesqId(int valor) {
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+		Query consulta = sessao
+				.createQuery("from FormaPgto where fpg_id = :parametro");
+		consulta.setInteger("parametro", valor);
+		sessao.close();
+		return (FormaPgto) consulta.uniqueResult();
+	}
+
+}
